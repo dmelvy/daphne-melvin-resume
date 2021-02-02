5d99fe1ce5f408fa234589bfca4ca4c8e35e293e
Wifey <wifey@Adrians-MacBook-Air.local>
2021-02-02 17:50:27 +0000

    colors package

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 #! /usr/bin/env node
 
+const colors = require('colors');
+// colors.enable();
+
+// colors.setTheme({
+//   silly: 'rainbow',
+//   input: 'grey',
+//   verbose: 'cyan',
+//   prompt: 'grey',
+//   info: 'green',
+//   data: 'grey',
+//   help: 'cyan',
+//   warn: 'yellow',
+//   debug: 'blue',
+//   error: 'red'
+// });
+
 const fs = require('fs')
 
 fs.readFile(__dirname + '/info.json', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
   } else {
-    console.log(data);
-    return data;
+    console.log(data.silly);
+    return data.silly;
   }
 })
